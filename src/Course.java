ff216d588b6e9a3d1cc858f3086957987061ef5a
Yangzhe Huang <yhuan385@uwo.ca>
2017-02-12 12:29:04 -0500

    Add course class and make changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ricardo on 2017-02-12.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname,String cdescription) {
+        this.cname = cname;
+        this.description = cdescription;
+    }
+}
